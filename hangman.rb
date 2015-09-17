dd1b13b44443e8852143ac1cae8aaafe9e879f81
jgisin <jgisin@gmail.com>
2015-09-17 11:53:02 -0600

    Difficulty Setting and Redirect to Game.erb implemented

--- hangman.rb
+++ hangman.rb
@@ -1,14 +1,50 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-word_length = ['_', '_', '_', '_']
-dictionary = File.open("enable.txt", "r")
-contents = dictionary.read
+word_length = ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_']
+arr = []
+random_num = rand(arr.length)
+contents = ""
+
 
 get '/' do
-	if params["guess"] != nil
-		word_length[0] = params["guess"]
-	end
+		if params["difficulty"] != nil
+			redirect('game')
+			set erb :game
+		end
+		difficulty = nil
+		if params["difficulty"] != nil
+			difficulty = params["difficulty"].to_i
+		end
+
+	dictionary = File.open("enable.txt", "r")
+	
+		while !dictionary.eof?
+			line = dictionary.readline
+				if line.length - 1 == difficulty
+					arr << line
+				end
+		end
+		contents = arr.join.split("\n")[rand(arr.length)].to_s
+			if params["difficulty"] != nil
+				word_length = word_length.take(contents.length)
+			end
+		dictionary.close
+
+		
+		#if params["guess"] != nil
+			#word_length[0] = params["guess"]
+		#end
+
 	erb :index, :locals => {:word_length => word_length,
 	:contents => contents}
+
 end
+
+get '/game' do
+	erb :game, :locals => {:word_length => word_length,
+	:contents => contents}
+end
+
+
+
